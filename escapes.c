38a53ce0b8ff948b31e19ceb9b22a83475f71be1
chaitraliKhanvilkar <35175966+chaitraliKhanvilkar@users.noreply.github.com>
2018-01-12 23:01:05 +0530

    Add files via upload

---
+++ escapes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+	printf("Newline:\n");
+	printf("tab:\t");
+	printf("\nVertical tab:\v");
+	printf("\nBackslash:\b");
+	printf("\nSlash:\\");
+	printf("\nDouble quotes:\"");
+	printf("\nSingle quote:\'");
+	printf("\nQuestion mark:\?");
+	printf("\r");
+}
