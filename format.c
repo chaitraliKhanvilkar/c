145284a83d813f480a7a9759c59360ed9980b653
chaitraliKhanvilkar <35175966+chaitraliKhanvilkar@users.noreply.github.com>
2018-01-17 23:06:27 +0530

    Add files via upload

---
+++ format.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+    printf("\nInteger:%d",10);
+    printf("\nCharacter:%c",'L');
+    printf("\nFloat:%f",10.234);
+    printf("\nDouble:%lf",243.67867);
+    printf("\nLong int:%ld",5345345);
+    printf("\nLong long int:%lld",534534545);
+    printf("\nUnsigned:%u",132);
+    printf("\nLong unsigned int:%lu",234432423);
+    printf("\nUnsigned character:%uc",'f');
+}
